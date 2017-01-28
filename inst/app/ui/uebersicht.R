872d2292a28d41baee95454934329d9848208389
Jansim <jan.simson@three-2-one.com>
2017-01-28 14:50:34 +0100

    Organized Uebersicht into some boxes

--- inst/app/ui/uebersicht.R
+++ inst/app/ui/uebersicht.R
@@ -1,10 +1,16 @@
 uebersicht <- tabItem(tabName = "uebersicht", 
   textOutput("test"),
-  plotOutput("ueb_scatter_groesse.miete"),
-  plotOutput("ueb_scatter_bewohner.mieteproqm"),
-  plotOutput("ueb_hist_count.bystadtteil"),
-  plotOutput("ueb_hist_bewohner.bystadtteil"),
-  plotOutput("ueb_hist_mieteproqm.bystadtteil"),
+  box(title = "Scatterplots", status = "primary", width = 12,
+    splitLayout(cellWidths = c("50%", "50%"),
+      plotOutput("ueb_scatter_groesse.miete"),
+      plotOutput("ueb_scatter_bewohner.mieteproqm")       
+    )
+  ),
+  tabBox(title = "Nach Stadtteil", width = 12,
+    tabPanel("Anzahl", plotOutput("ueb_hist_count.bystadtteil")),
+    tabPanel("Bewohner", plotOutput("ueb_hist_bewohner.bystadtteil")),
+    tabPanel("Miete/m²", plotOutput("ueb_hist_mieteproqm.bystadtteil"))
+  ),
   pierOutput("ueb_pie"),
   pierOutput("ueb_pie_gesucht")
 )
