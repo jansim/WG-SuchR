cdc861e7f1ac29146fdeb0a3c0596ed2d913e5ec
Jansim <jan.simson@three-2-one.com>
2017-01-28 16:08:36 +0100

    Fixed übersicht height

--- inst/app/ui/uebersicht.R
+++ inst/app/ui/uebersicht.R
@@ -1,21 +1,23 @@
 uebersicht <- tabItem(tabName = "uebersicht", 
   textOutput("test"),
-  box(title = "Scatterplots", status = "primary", width = 12,
-    splitLayout(cellWidths = c("50%", "50%"),
-      plotOutput("ueb_scatter_groesse.miete"),
-      plotOutput("ueb_scatter_bewohner.mieteproqm")       
-    )
-  ),
-  tabBox(title = tagList(shiny::icon("bar-chart"), "Nach Stadtteil"), width = 12,
-    tabPanel("Anzahl", plotOutput("ueb_hist_count.bystadtteil")),
-    tabPanel("Bewohner", plotOutput("ueb_hist_bewohner.bystadtteil")),
-    tabPanel("Miete/m²", plotOutput("ueb_hist_mieteproqm.bystadtteil")),
-    "Test"
-  ),
-  box(title = "Pies", status = "warning", width = 12,
-    splitLayout(cellWidths = c("50%", "50%"),
-      plotOutput("ueb_pie"),
-      plotOutput("ueb_pie_gesucht")
-    )
+  fluidRow(
+    box(title = "Scatterplots", status = "primary", width = 12,
+      splitLayout(cellWidths = c("50%", "50%"),
+        plotOutput("ueb_scatter_groesse.miete", height = 210),
+        plotOutput("ueb_scatter_bewohner.mieteproqm", height = 210)       
+      )
+    ),
+    tabBox(title = tagList(shiny::icon("bar-chart"), "Nach Stadtteil"), width = 12,
+      tabPanel("Anzahl", plotOutput("ueb_hist_count.bystadtteil")),
+      tabPanel("Bewohner", plotOutput("ueb_hist_bewohner.bystadtteil")),
+      tabPanel("Miete/m²", plotOutput("ueb_hist_mieteproqm.bystadtteil")),
+      "Test"
+    ),
+    box(title = "Pies", status = "warning", width = 12,
+      splitLayout(cellWidths = c("50%", "50%"),
+        plotOutput("ueb_pie", height = 200),
+        plotOutput("ueb_pie_gesucht", height = 200)
+      )
+    ) 
   )
 )
