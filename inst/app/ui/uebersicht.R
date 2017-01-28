2f669fbcb2f8ea3a543db36148082630914dc0dc
Jansim <jan.simson@three-2-one.com>
2017-01-28 17:36:01 +0100

    minor imporvements

--- inst/app/ui/uebersicht.R
+++ inst/app/ui/uebersicht.R
@@ -1,13 +1,13 @@
 uebersicht <- tabItem(tabName = "uebersicht", 
   textOutput("test"),
   fluidRow(
-    box(title = "Scatterplots", status = "primary", width = 12,
+    box(title = "Verteilung", status = "primary", width = 12, collapsible = T,
       splitLayout(cellWidths = c("50%", "50%"),
         plotOutput("ueb_scatter_groesse.miete", height = 210),
         plotOutput("ueb_scatter_bewohner.mieteproqm", height = 210)       
       )
     ),
-    tabBox(title = tagList(shiny::icon("bar-chart"), "Nach Stadtteil"), width = 9,
+    tabBox(title = tagList(shiny::icon("bar-chart"), "nach Stadtteilen"), width = 9,
       tabPanel("Anzahl", plotOutput("ueb_hist_count.bystadtteil")),
       tabPanel("Bewohner", plotOutput("ueb_hist_bewohner.bystadtteil")),
       tabPanel("Miete/m²", plotOutput("ueb_hist_mieteproqm.bystadtteil")),
@@ -17,7 +17,7 @@ uebersicht <- tabItem(tabName = "uebersicht",
     box(width = 3,
       checkboxInput("ueb_hist_order", "Histogramme ordnen")
     ),
-    box(title = "Pies", status = "warning", width = 12,
+    box(title = "Geschlechterverhältnis", status = "warning", width = 12, collapsible = T,
       splitLayout(cellWidths = c("50%", "50%"),
         plotOutput("ueb_pie", height = 200),
         plotOutput("ueb_pie_gesucht", height = 200)
