4f879de635a9ac9d3e51c9664715df64aac583ab
Jan Simson <jan.simson@three-2-one.com>
2017-05-13 17:48:21 +0200

    Revert "Moved init.R into R folder"
    This reverts commit 340bcd6448c4484562bfa465f5602a442f7390b7.

--- R/init.R
+++
@@ -1,7 +0,0 @@
-source("data.R", chdir = T)
-source("pie_charts.R", chdir = T)
-source("scatterplots.R", chdir = T)
-source("hist.R", chdir = T)
-
-staedte <- load.cities()
-staedte.list <- cities.to.list(staedte)
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,5 +1,5 @@
 # Load functions
-source("../../R/init.R", chdir = T)
+source("../../init.R", chdir = T)
 
 # Load Tab Contents
 source("ui/uebersicht.R")
